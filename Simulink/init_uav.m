b97003f5af6387485b28f1ecd5ed0950823f16be
Federico Oliva <lvofrc95@gmail.com>
2021-05-05 01:17:12 +0200

    misero tentativo simulink

---
+++ Simulink/init_uav.m
@@ -0,0 +1,72 @@
+%% params definition
+% define simulation time
+params.time=0:1e-3:2;
+
+% init angles (euler ZYX)
+eul_0=transpose([0; 0; 0]);
+% convert to quaternion
+q_start=eul2quat(eul_0);
+
+% init state
+params.w0=[q_start(1); q_start(2); q_start(3); q_start(4); 0; 0; 0]; %7x1 [q0 q1 q2 q3 p q r] quaternioni e velocità angolari (pqr sistema di riferimento ABC)
+params.s0=[3; 3; -3; 0;0;0]; %6x1 [x y z u v w] xyz sistema di riferimento NED, uvw sistema di riferimento ABC
+
+% define input
+params.tau_story = [0; 0; 0].*ones(3,length(params.time));
+
+% mass
+params.m = 0.5;
+
+% inertia matrix
+params.I=[0.0081 0 0; ...
+          0 0.0081 0;...
+          0 0 0.0162];
+      
+%lunghezza braccio dal centro di massa
+params.l=0.2;  
+
+%coefficiente di spinta
+params.b=1.46e-5;   
+
+% currently BOOH
+params.k=0.026;
+
+% gravity
+params.g=0*9.81; 
+
+%Drag factor
+params.d=0.0443;       
+
+%parametri motori
+params.tau_mot=0.1;    
+
+params.omega0=323;
+% params.omegaThrust=1*[200; -200; 200; -200];
+
+% params.omegaThrust=[277;... %Caso a) ROLL  (+ ROTORE 1 , -ROTORE3)
+%     -267;...
+%     257;...
+%     -267];
+    
+% params.omegaThrust=[267;... %Caso b) PITCH (+ ROTORE 2 , -ROTORE4)
+%     277;...
+%     267;...
+%     257];
+   
+params.omegaThrust=[367;... %Caso c) YAW (+ ROTORE1/ROTORE3 , - ROTORE2/ROTORE4)
+    267;...
+    367;...
+    267];
+
+%% simulink structure
+elems(1) = Simulink.BusElement;
+elems(1).Name = 'mass';
+elems(2) = Simulink.BusElement;
+elems(2).Name = 'Gravity';
+SineBus = Simulink.Bus;
+SineBus.Elements = elems;
+    
+    
+
+      
+      
---
+++ Simulink/synthetic_integration_v1.m
@@ -0,0 +1,130 @@
+%% model integration - synthetic model
+
+close all
+clear all
+
+addpath(genpath([pwd '\Lib']))
+
+% define simulation time
+params.time=0:1e-3:2;
+
+% init angles (euler ZYX)
+eul_0=transpose([0; 0; 0]);
+% convert to quaternion
+q_start=eul2quat(eul_0);
+
+% init state
+params.w0=[q_start(1); q_start(2); q_start(3); q_start(4); 0; 0; 0]; %7x1 [q0 q1 q2 q3 p q r] quaternioni e velocità angolari (pqr sistema di riferimento ABC)
+params.s0=[3; 3; -3; 0;0;0]; %6x1 [x y z u v w] xyz sistema di riferimento NED, uvw sistema di riferimento ABC
+
+% define input
+params.tau_story = [0; 0; 0].*ones(3,length(params.time));
+
+%define model
+init_uav;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                     INTEGRATION LOOP
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% init procedure
+%%% init state %%%
+
+pos_start =params.s0; %POSITION
+params.ned_story=pos_start(1:3);
+
+angle_start = params.w0; %ATTITUDE
+params.eul_story=quat2eul(angle_start(1:4)')';
+params.omega_story=angle_start(5:7);
+
+
+% start integration
+disp('STARTING INTEGRATION PROCEDURE')
+
+for i = 2:length(params.time)
+    params.tau=params.tau_story(:,i);
+    tspan=[params.time(i-1),params.time(i)];
+
+    % integration     
+    temp =ode45(@(t,w)AttitudeDynamics_v1(t,w,params),tspan,angle_start(:,i-1));  
+    angle_start(:,i) = temp.y(:,end);
+
+    % quaternioni to eulero
+    params.eul_story(:,i) = quat2eul(angle_start(1:4,i)');
+    params.eul = params.eul_story(:,i);        
+
+    params.omega_story(:,i)=angle_start(5:7,i);
+    params.omega= params.omega_story(:,i);
+
+    %%% integrate position 
+    temp =ode45(@(t,s)PositionDynamics_v2(t,s,params),tspan,pos_start(:,i-1));  
+    pos_start(:,i) = temp.y(:,end);
+
+    params.ned_story(:,i)=pos_start(1:3,i);
+    params.vel_story(:,i)=pos_start(4:6,i);
+
+end
+
+%plot2 XYZ   
+figure
+hold on
+grid on
+plot(params.time,params.ned_story(1,:))
+plot(params.time,params.ned_story(2,:))
+plot(params.time,params.ned_story(3,:))
+lgd=legend('X','Y','Z');
+title(lgd,'Coordinate NED')  
+
+
+%plot2 velocità lineari UVW
+figure
+hold on
+grid on
+plot(params.time,params.vel_story(1,:))
+plot(params.time,params.vel_story(2,:))
+plot(params.time,params.vel_story(3,:))
+lgd=legend('U','V','W');
+title(lgd,'Velocità lineari su XYZ')    
+
+
+
+
+%plot2 angoli di eulero
+figure 
+hold on
+grid on
+plot(params.time,params.eul_story(1,:))
+plot(params.time,params.eul_story(2,:))
+plot(params.time,params.eul_story(3,:))
+lgd=legend('phi','theta','psi');
+title(lgd,'Angoli di Eulero')
+
+
+
+%plot2 velocità angolari
+figure
+hold on
+grid on
+plot(params.time,params.omega_story(1,:))
+plot(params.time,params.omega_story(2,:))
+plot(params.time,params.omega_story(3,:))
+lgd=legend('p','q','r');
+title(lgd,'Velocità angolari')
+
+%plot3 andamento della posizione%
+figure
+hold on
+grid on
+plot3(params.ned_story(1,:),params.ned_story(2,:),params.ned_story(3,:))
+plot3(params.ned_story(1,1),params.ned_story(2,1),params.ned_story(3,1),'ro')
+plot3(params.ned_story(1,end),params.ned_story(2,end),params.ned_story(3,end),'r+')
+legend('Posizione')
+
+
+
+
+
+
+
+
+
+
